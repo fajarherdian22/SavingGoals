3a42611d326ffeb496c0ed768899bc1f054e721e
Fajar Herdian <fajarherdian22@gmail.com>
2023-01-28 19:07:37 +0700

    V3

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
 var arr = []
-var month = [] /// cuma bisa sampe 12
+var month = []
 var data = []
-
+var bulanan = []
 
 function compoundInvestment(InitialDepo, rate, months, monthlyAddition) {
     var total = InitialDepo;
+    //
     for (let i = 1; i <= months; i++) {
         total += monthlyAddition;
         total = Math.floor(total * (1 + rate / 12));
         arr.push(total)
     }
+    // membuat variable return bulanan
+    for(i=0; i< arr.length; i++){
+        var x = Math.floor(arr[i]*(rate/12))
+        bulanan.push(x)
+    }
+    //
     for(i=1; i<= arr.length; i++){
         month.push(i)
     }
 
+    //  membuat array object bulan dan returns
     for (let i = 0; i < arr.length; i++) {
-        data.push({ month: month[i], amount: arr[i] });
+        data.push({ month: month[i], amount: arr[i], ROI: bulanan[i]});
     }
+    // parsing ke HTML
     const table = document.querySelector('tbody')
     data.forEach((hasil) => {
         table.innerHTML = table.innerHTML + `<tr>
                 <td>${hasil.month}</td>
                 <td>${hasil.amount}</td>
+                <td>${hasil.ROI}</td>
             </tr>`
     })
-
     return total;
 }
 
@@ -36,26 +45,27 @@ form.addEventListener("submit", function (e) {
     const months = parseInt(document.getElementById("months").value);
     const monthlyAddition = parseInt(document.getElementById("monthly-addition").value);
     const result = compoundInvestment(principal, rate, months, monthlyAddition);
-    console.log(result);
-    console.log(arr)
+    console.log(arr);
+    // console.log(result);
+    console.log(bulanan);
     console.log(data);
 });
 
 
-// const newTable = document.createElement("table");
-// newTable.innerHTML = "<thead><th>Month</th><th>Return</th></thead>";
-
-// for(Data of data){
-//     const newRow = document.createElement("tr")
-//     const tdMonth = document.createElement("td")
-//     const tdReturn = document.createElement("td")
-//     tdMonth.textContent = Data.month;
-//     tdReturn.textContent = Data.amount;
-//     newRow.appendChild(tdMonth);
-//     newRow.appendChild(tdReturn);
-//     newTable.appendChild(newRow);
-// }
-
-// const target = document.getElementById('target');
-// target.appendChild(newTable);
-
+// new Chart("myChart", {
+//     type: "line",
+//     data: {
+//       labels: month,
+//       datasets: [{
+//         backgroundColor: "rgba(0,0,0,1.0)",
+//         borderColor: "rgba(0,0,0,0.1)",
+//         data: arr
+//       }]
+//     },
+//     options: {
+//         legend: {display: false},
+//         scales: {
+//           yAxes: [{ticks: {min: 6, max:16}}],
+//         }
+//       }
+//     });
