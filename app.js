159927fa57dd4c2ec40d82c0d66d73d6f3e0dfec
Fajar Herdian <fajarherdian22@gmail.com>
2023-01-29 15:07:25 +0700

    V4

--- app.js
+++ app.js
@@ -5,33 +5,65 @@ var bulanan = []
 
 function compoundInvestment(InitialDepo, rate, months, monthlyAddition) {
     var total = InitialDepo;
-    //
+
+    // Compound invest calc
     for (let i = 1; i <= months; i++) {
         total += monthlyAddition;
         total = Math.floor(total * (1 + rate / 12));
         arr.push(total)
     }
     // membuat variable return bulanan
-    for(i=0; i< arr.length; i++){
-        var x = Math.floor(arr[i]*(rate/12))
+    for (i = 0; i < arr.length; i++) {
+        var x = Math.floor(arr[i] * (rate / 12))
         bulanan.push(x)
     }
-    //
-    for(i=1; i<= arr.length; i++){
+    // Membuat Variable bulan
+    for (i = 1; i <= arr.length; i++) {
         month.push(i)
     }
+    var formater = new Intl.NumberFormat(undefined, { style: 'currency', currency: 'IDR' })
+    var formatted = arr.map(formater.format);
+
+    
+    var formater2 = new Intl.NumberFormat(undefined, { style: 'currency', currency: 'IDR' })
+    var formatted2 = bulanan.map(formater2.format);
 
     //  membuat array object bulan dan returns
     for (let i = 0; i < arr.length; i++) {
-        data.push({ month: month[i], amount: arr[i], ROI: bulanan[i]});
+        data.push({ month: month[i], amount: formatted[i], ROI: formatted2[i] });
     }
+    // Membuat Chart
+    new Chart(document.getElementById("bar-chart"), {
+        type: 'bar',
+        data: {
+            labels: month,
+            datasets: [
+                {
+                    label: "Total Returns (RP) ",
+                    backgroundColor: "#3cba9f",
+                    data: arr
+                }
+            ]
+        },
+        options: {
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Grafik investings with rate (RP)',
+                type: 'logarithmic'
+            }
+        }
+    });
+
+
+
     // parsing ke HTML
     const table = document.querySelector('tbody')
     data.forEach((hasil) => {
-        table.innerHTML = table.innerHTML + `<tr>
-                <td>${hasil.month}</td>
-                <td>${hasil.amount}</td>
-                <td>${hasil.ROI}</td>
+        table.innerHTML = table.innerHTML + `<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <td class="px-6 py-4">${hasil.month}</td>
+                <td class="px-6 py-4">${hasil.amount}</td>
+                <td class="px-6 py-4">${hasil.ROI}</td>
             </tr>`
     })
     return total;
@@ -45,27 +77,6 @@ form.addEventListener("submit", function (e) {
     const months = parseInt(document.getElementById("months").value);
     const monthlyAddition = parseInt(document.getElementById("monthly-addition").value);
     const result = compoundInvestment(principal, rate, months, monthlyAddition);
-    console.log(arr);
     // console.log(result);
-    console.log(bulanan);
     console.log(data);
 });
-
-
-// new Chart("myChart", {
-//     type: "line",
-//     data: {
-//       labels: month,
-//       datasets: [{
-//         backgroundColor: "rgba(0,0,0,1.0)",
-//         borderColor: "rgba(0,0,0,0.1)",
-//         data: arr
-//       }]
-//     },
-//     options: {
-//         legend: {display: false},
-//         scales: {
-//           yAxes: [{ticks: {min: 6, max:16}}],
-//         }
-//       }
-//     });
